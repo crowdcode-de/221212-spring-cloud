ebbbd697b5d62d2882e6acf3fdff699bbd712767
Ingo Düppe <ingo.dueppe@crowdcode.io>
2022-12-12 16:43:08 +0100

    solution 09 eureka missing dependency 2.0.0-RC4

---
+++ cluster-services/discovery-server/src/main/java/io/crowdcode/cloudbay/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,16 @@
+package io.crowdcode.cloudbay.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ingo Düppe (CROWDCODE)
+ */
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
