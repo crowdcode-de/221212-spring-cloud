c61b6a4a9ee90d0068eab2977c19f54584d8ab0f
Ingo Düppe <ingo.dueppe@crowdcode.io>
2022-12-12 14:32:34 +0100

    solution 03 add admin server

---
+++ cluster-services/admin-server/src/main/java/io/crowdcode/cloudbay/adminserver/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package io.crowdcode.cloudbay.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ingo Düppe (CROWDCODE)
+ */
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
