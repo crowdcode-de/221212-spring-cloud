d48ecc1c6bdd7d1f37009b8af8290b8c881702a0
Ingo Düppe <ingo.dueppe@crowdcode.io>
2022-12-29 21:32:28 +0100

    Solution 11 - add observation

--- domain-services/time-service/src/main/java/io/crowdcode/cloudbay/timeservice/web/TimeController.java
+++ domain-services/time-service/src/main/java/io/crowdcode/cloudbay/timeservice/web/TimeController.java
@@ -1,6 +1,7 @@
 package io.crowdcode.cloudbay.timeservice.web;
 
 import io.crowdcode.cloudbay.common.AnsiColor;
+import io.micrometer.observation.annotation.Observed;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +13,14 @@ import java.time.LocalDateTime;
 public class TimeController {
 
     @GetMapping("/now")
+//    @Observed(
+//            name = "time calculation",
+//            contextualName = "",
+//            lowCardinalityKeyValues = {"class.name", "TimeController"}
+//    )
     public TimeResponse now() {
         TimeResponse timeResponse = new TimeResponse().setNow(LocalDateTime.now());
-        log.info(AnsiColor.blue("GOT REQUEST AND SAY "+timeResponse.getNow()));
+        log.info(AnsiColor.blue("GOT REQUEST AND SAY " + timeResponse.getNow()));
         return timeResponse;
     }
 }
